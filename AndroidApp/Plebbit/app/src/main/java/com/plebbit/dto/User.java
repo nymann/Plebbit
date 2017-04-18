cf0cf69ffa769e43c2c353c1404425c954ff4576
Morten J <morten2094@hotmail.com>
2017-04-18 12:38:12 +0200

    lmao auck fndroid

---
+++ AndroidApp/Plebbit/app/src/main/java/com/plebbit/dto/User.java
@@ -0,0 +1,13 @@
+package com.plebbit.dto;
+
+/**
+ * Created by Morten on 4/18/2017.
+ */
+
+public class User {
+
+    public int userId;
+    public String name;
+    public String token;
+    public String time;
+}
